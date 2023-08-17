c6b91a7b92a8c2ba2fb1a6f3b321fd048bc343ff
Aruna Gungi <aruna.gungi98@example.com>
2023-08-17 17:29:17 +0530

    Example for controlled and uncontrolled components using ref and usestate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useCallback, useRef, useState } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const usernameRef = useRef();
+  const passwordRef = useRef();
+
+  // const handleInputChange = (e) => {
+  //   setData({ ...data, [e.target.name]: e.target.value });
+  // };
+
+  const handleSubmit = useCallback(() => (event) => {
+    event.preventDefault();
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    setData({ username, password });
+  });
+
+  // const handleSubmit = () => {
+  //   console.log("Data", data);
+  // }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter username"
+          // onChange={(e) => handleInputChange(e)}
+          ref={usernameRef}
+        ></input>
+        <br></br>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter the password"
+          // onChange={(e) => handleInputChange(e)}
+          ref={passwordRef}
+        ></input>
+        <br></br>
+        <button type="submit" onClick={handleSubmit()}>
+          Submit
+        </button>
+      </form>
+
+      {data && <p>Hello {data.username}!</p>}
     </div>
   );
 }
